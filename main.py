f2ced99e5ea35f8698080b8b75fe904d5f274f17
newmarwegner <newmarwegner@gmail.com>
2021-03-31 11:15:55 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import psycopg2
+from decouple import config
+
+# function to connect with a database postgresql
+def con_postgres(dbname='postgres', user='postgres'):
+
+    return psycopg2.connect(f"dbname={config('dbname')} user={config('user')} host={config('host')} "
+                            f"password={config('password')} port={config('port')}")
+
+# function to refresh materialized views
+# needs  .env file with materialized views separated by quote
+def refresh_vm():
+    views_to_refresh = config('vm_to_refresh').split(",")
+    conn = con_postgres()
+    cur = conn.cursor()
+    
+    for vm in views_to_refresh:
+        sql = f"refresh materialized view concurrently {vm};"
+        cur.execute(sql)
+    
+    conn.close()
+    
+    return
+
+if __name__ == '__main__':
+    refresh_vm()
+    print('Materialized views refreshed!')
